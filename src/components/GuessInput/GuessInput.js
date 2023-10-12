e1d6a8a33e092102995cd8b3a138a68bb5e976ea
Iryna Howarth <irynahowarth@gmail.com>
2023-10-12 13:59:17 +0100

    Excersice1: GuessInput - Done

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function formHandler(e) {
+    e.preventDefault();
+    console.log({ guess: guess });
+    setGuess("");
+  }
+
+  return (
+    <>
+      <form className="guess-input-wrapper" onSubmit={formHandler}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          value={guess}
+          pattern="\w{5,5}"
+          onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        />
+      </form>
+    </>
+  );
+}
+
+export default GuessInput;
